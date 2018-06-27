2ac0e228947aa96a56aa536075822ac98cfb2f48
Vladimir Pavković <vladimirpavk@gmail.com>
2018-06-27 15:24:02 +0200

    +

--- src/app/store/item-list.action.ts
+++ src/app/store/item-list.action.ts
@@ -5,6 +5,7 @@ import { Item } from '../models/item.model';
 export const LOAD_ITEMS:string = 'LOAD_ITEM';
 export const LOAD_ITEMS_SUCCESS:string = 'LOAD_ITEM_SUCCESS';
 export const LOAD_ITEMS_FAIL:string = 'LOAD_ITEM_FAIL';
+export const ADD_ITEM:string = 'ADD_ITEM';
 
 export class LoadItems implements Action{
     public readonly type:string = 'LOAD_ITEMS';
@@ -18,6 +19,11 @@ export class LoadItemsSuccess implements Action{
 export class LoadItemsFail implements Action{
     public readonly type:string = 'LOAD_ITEMS_FAIL';
     constructor(public payload:any){}
+}
+
+export class AddItem implements Action{
+    public readonly type:string = 'ADD_ITEM';
+    constructor(public payload:Item){}
 }
 
 export type ItemsActions = LoadItems | LoadItemsFail | LoadItemsSuccess;
---
+++ src/app/store/item-list.reducer.ts
@@ -0,0 +1,26 @@
+import { Item } from "../models/item.model";
+import * as ItemListAction from './item-list.action';
+
+export interface ItemListState{
+    data:Item[]    
+};
+
+export const initialState:ItemListState = {
+    data:[]
+};
+
+export function reducer(state=initialState, action:ItemListAction.ItemsActions):ItemListState{
+    switch(action.type){
+        case ItemListAction.LOAD_ITEMS:{
+            return {
+                ...state
+            }
+        }
+        case ItemListAction.ADD_ITEM:{
+            return{                
+                data:[...state.data, (<ItemListAction.AddItem>action).payload]
+            }
+        }
+    }
+    return state;
+}
