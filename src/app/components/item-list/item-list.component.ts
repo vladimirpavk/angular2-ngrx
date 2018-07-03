de175cf35195b604f8f71602cc591b1f9f63040e
Vladimir Pavković <vladimirpavk@gmail.com>
2018-07-03 10:02:00 +0200

    Finished example

--- src/app/components/item-list/item-list.component.ts
+++ src/app/components/item-list/item-list.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Item } from '../../models/item.model';
+import { Store } from '@ngrx/store';
+import { ItemListState } from '../../store/reducers/item-list.reducer';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-item-list',
@@ -8,24 +11,30 @@ import { Item } from '../../models/item.model';
 })
 export class ItemListComponent implements OnInit {
 
-  private itemList:Item[] = [];
+//  private itemList:Item[] = [];
+  private itemList:Observable<ItemListState>;
 
   private itemName:string;
   private itemSize:number;
-
-  constructor() { }
+  
+  constructor(public store:Store<ItemListState>) { }
 
   ngOnInit() {
-    this.itemList=[
+    /*this.itemList=[
       new Item("Peskir", 20),
       new Item("Papuče", 5),
       new Item("Igračke", 25)
-    ]
+    ]*/
+    this.itemList=this.store.select('itemList');
+
+    this.itemList.subscribe((data:ItemListState)=>{
+      console.log(data);
+    })
   }
 
   public insert():void{
     //console.log(this.itemName+" - "+this.itemSize);
-    this.itemList=[...this.itemList, new Item(this.itemName, this.itemSize)];
+    //this.itemList=[...this.itemList, new Item(this.itemName, this.itemSize)];
   }
 
 }
--- src/app/store/reducers/reducers.ts
+++
@@ -1,8 +0,0 @@
-import * as ItemListReducer from './item-list.reducer';
-import * as PeopleListReducer from './people-list.reducer';
-
-export interface AppState{
-    items : ItemListReducer.ItemListState,
-    people : PeopleListReducer.PeopleListState
-}
-
